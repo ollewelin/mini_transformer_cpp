d2db0df1faa86f1894c3b8064866fa8e08b9e924
Olle Welin <olle_welin@hotmail.com>
2024-12-26 21:41:38 +0100

    Delete src directory

--- src/embedding.h
+++
@@ -1,9 +0,0 @@
-#include <vector>
-class Embedding {
-public:
-    Embedding(int vocab_size, int d_model);
-    std::vector<std::vector<float>> forward(const std::vector<int>& input);
-
-private:
-    std::vector<std::vector<float>> embedding_matrix;
-};
--- src/utils.h
+++
@@ -1,6 +0,0 @@
-#include <vector>
-namespace Utils {
-    std::vector<std::vector<float>> matmul(const std::vector<std::vector<float>>& a, const std::vector<std::vector<float>>& b);
-    std::vector<std::vector<float>> transpose(const std::vector<std::vector<float>>& matrix);
-    std::vector<float> softmax(const std::vector<float>& input);
-};

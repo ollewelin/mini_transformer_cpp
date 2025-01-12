d3ffe684ded45b58416d74960fe85aa873d886a7
Olle Welin <olle_welin@hotmail.com>
2025-01-12 19:53:35 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -785,7 +785,7 @@ int main() {
         
 
     // ============== Training loop ===================
-    int epochs = 1000;
+    int epochs = 5000;
     // Initialize velocity for weights and bias
     std::vector<std::vector<float>> velocity_weights(final_weights.size(),
                                                      std::vector<float>(final_weights[0].size(), 0.0f));

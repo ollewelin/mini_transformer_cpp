9f8a951d35346c3aac9d974bd142202cec0cc1b8
Olle Welin <olle_welin@hotmail.com>
2025-01-12 15:54:17 +0100

    Add files via upload

--- include/config.h
+++ include/config.h
@@ -13,7 +13,6 @@
 extern float GLOBAL_LEAKY_SLOPE;
 extern float GLOBAL_learning_rate;
 extern float GLOBAL_momentum ;
-
 extern float GLOBAL_ATTENTION_learning_rate;
 extern float GLOBAL_ATTENTION_momentum ;
 extern const float GLOBAL_CONST_learning_rate;

f4b761d37804ea11679122427a8defdfdfc89d38
Olle Welin <olle_welin@hotmail.com>
2025-01-12 12:55:55 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -761,8 +761,8 @@ int main() {
                                                      std::vector<float>(final_weights[0].size(), 0.0f));
     std::vector<float> velocity_bias(final_bias.size(), 0.0f);
 
-    GLOBAL_learning_rate = 0.0001;
-    GLOBAL_momentum = 0.1;
+    GLOBAL_learning_rate = 0.00003;
+    GLOBAL_momentum = 0.9;
     GLOBAL_ATTENTION_learning_rate = GLOBAL_learning_rate;
     GLOBAL_ATTENTION_momentum = GLOBAL_momentum;   
     std::cout << "learning_rate: " << GLOBAL_learning_rate << std::endl;

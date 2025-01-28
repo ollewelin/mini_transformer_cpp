80cb9532533ede47e03604a80e50d62311a1ce8b
Olle Welin <olle_welin@hotmail.com>
2025-01-28 23:44:57 +0400

    Add files via upload

--- many_categories/src/main.cpp
+++ many_categories/src/main.cpp
@@ -525,7 +525,7 @@ int main()
                                                          std::vector<float>(num_categories, 0.0f));
         std::vector<float> velocity_bias(num_categories, 0.0f);
 
-        GLOBAL_learning_rate = 0.0025f;
+        GLOBAL_learning_rate = 0.0001f;
         GLOBAL_momentum = 0.9f;
         GLOBAL_ATTENTION_learning_rate = GLOBAL_learning_rate;
         GLOBAL_ATTENTION_momentum = GLOBAL_momentum;

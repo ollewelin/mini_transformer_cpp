a139d02451c9babdd8720cb7baa2a25f066e6e63
Olle Welin <olle_welin@hotmail.com>
2025-02-23 23:22:33 +0400

    Add files via upload

--- include/dataset.h
+++ include/dataset.h
@@ -16,8 +16,8 @@ std::vector<int> tokenize(const std::string &sentence, const std::unordered_map<
 bool load_vocab_from_file(const std::string &vocab_file, std::unordered_map<std::string, int> &vocab);
 
 // New function to prepare dataset from question.txt and answer.txt
-bool prepare_dataset_from_files(const std::string &question_file,
-                                const std::string &answer_file,
+bool prepare_dataset_from_files(const std::vector<std::string> &input_files,
+                                const std::unordered_map<std::string, int> &label_map,
                                 std::vector<std::vector<int>> &data,
                                 std::vector<int> &labels,
                                 const std::unordered_map<std::string, int> &vocab);
